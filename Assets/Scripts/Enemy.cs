d01225d429563cb69615f40b9f80fecaaee90bd2
kailermai <kailermaioff@gmail.com>
2024-03-07 23:17:57 +0800

    added damage animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,11 +9,15 @@ public class Enemy : MonoBehaviour
     public int maxHp;
     public int goldToGive;
     public Image healthBarFill;
+    public Animation anim;
 
     public void Damage()
     {
         curHp--;
         healthBarFill.fillAmount = (float)curHp / (float)maxHp;
+
+        anim.Stop();
+        anim.Play();
 
         if (curHp <= 0)
         {
